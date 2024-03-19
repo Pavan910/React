2f504ddf094d9e75097a66c43eefd8a96efefdfc
Pavan Singh <87536471+Pavan910@users.noreply.github.com>
2024-03-19 21:57:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import World from "./World";
+function App() {
+  return (
+    <>
+    <World/>
+        <h1>Hello world!</h1>
+        </>
+  );
+}
+
+export default App;
---
+++ src/World.js
@@ -0,0 +1,6 @@
+function World(){
+return(
+<h2>World is round</h2>
+);
+}
+export default World
